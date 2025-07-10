3b609b5776400abdb32de9f1966a169cc4c99280
thabang165 <atlemola@gmail.com>
2025-07-10 10:33:28 +0200

    initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 65
+number2 = 26
+
+addtion = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print("Addition of",number1 ,"and", number2, "is", addtion)
+print("Subtraction of",number1 ,"and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
