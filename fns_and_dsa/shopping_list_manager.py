815d0e2cadc60aa1a079c50e45d9d2b02f7b9324
thabang165 <atlemola@gmail.com>
2025-07-26 13:40:57 +0200

    updating the enter item propmt

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ").strip()
 
         if choice == '1':
-            item = input("Enter item to add: ").strip()
+            item = input("Enter item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to the list.")
 
