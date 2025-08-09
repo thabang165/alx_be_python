92e73aca70ff434e4577c48c2aaf217a8e7dd2a4
thabang165 <atlemola@gmail.com>
2025-08-09 11:59:13 +0200

    first commit

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+# book_class.py
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book({self.title!r}, {self.author!r}, {self.year!r})"
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
